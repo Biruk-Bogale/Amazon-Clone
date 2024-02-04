70da70e1a644505168cee82de689550b5e709ea5
Biruk-Bogale <bbuurraa10@gmail.com>
2024-02-04 19:52:20 +0300

    Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Carousel1 from "./Components/Carousel/Carousel1";
 
 function App() {
   return (
     <div>
       <Header />
+      <Carousel1 />
     </div>
   );
 }
