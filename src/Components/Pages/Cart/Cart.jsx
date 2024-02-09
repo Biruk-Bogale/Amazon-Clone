311621a13bd177798213d6d2db17820ac21f4bc4
Biruk Bogale <bbuurraa10@gmail.com>
2024-02-09 12:47:26 +0300

    cart part 2

--- src/Components/Pages/Cart/Cart.jsx
+++ src/Components/Pages/Cart/Cart.jsx
@@ -10,8 +10,10 @@ function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext);
 
   const total = basket.reduce((amount, item) => {
-    return item.price + amount;
+    return item.price * item.amount + amount;
   }, 0);
+
+  console.log(basket)
 
   return (
     <LayOut>
