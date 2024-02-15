6948576fabf21657a2526a04b6849184c01e2bb9
Biruk Bogale <bbuurraa10@gmail.com>
2024-02-15 16:39:34 +0300

    amazon server on render

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-6e87e/us-central1/api",
+  // local instace of firebase instance
+  // baseURL: "http://127.0.0.1:5001/clone-6e87e/us-central1/api",
+
+  // deployed version of amazon server on render.com
+  baseURL:"https://amazon-clone-backend-api-deploy.onrender.com"
 });
 
 export {axiosInstance}
